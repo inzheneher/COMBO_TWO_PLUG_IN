ef96cbadbbab1f0264e7113ae56707d5ad9672d9
Alexander Melnikov <inzheneher@ya.ru>
2016-05-26 00:40:33 +0600

    two plug-in experiment

---
+++ mav.composite.plugin.ui/src/mav/composite/plugin/ui/IAbstractConfigurationTreeNode.java
@@ -0,0 +1,10 @@
+package mav.composite.plugin.ui;
+
+import org.eclipse.jface.viewers.ICheckStateProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+
+abstract class IAbstractConfigurationTreeNode implements  
+				ILabelProvider, ITreeContentProvider, ICheckStateProvider {
+
+}
---
+++ mav.composite.plugin.ui/src/mav/composite/plugin/ui/ILaunchConfigurationTreeNode.java
@@ -0,0 +1,18 @@
+package mav.composite.plugin.ui;
+
+import org.eclipse.swt.graphics.Image;
+
+interface ILaunchConfigurationTreeNode {
+	ILaunchConfigurationTreeNode[] getChildren();
+	ILaunchConfigurationTreeNode getParent();
+	boolean hasChildren();
+	boolean hasSelectedChildren();
+
+	Image getImage();
+	String getText();
+
+	boolean isChecked();
+	boolean isGrayed();
+
+	void setCheckState(boolean state);
+}
